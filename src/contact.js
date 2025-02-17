8892982f6274e5852a07bf9567fe577f42ff57a4
Roope Kärkkäinen <roopeak@protonmail.com>
2025-02-17 11:24:10 +0200

    Logic to open contact page when contact button is clicked

---
+++ src/contact.js
@@ -0,0 +1,25 @@
+function generateContact() {
+	const contact = document.createElement('div');
+	const header = document.createElement('h1');
+	header.textContent = 'Contact Us';
+	contact.appendChild(header);
+
+	const contactCard = document.createElement('div');
+	contactCard.innerHTML = `
+		<h2>Phone number</h2>
+		<p>050 123 4567</p>
+		<h2>E-mail</h2>
+		<p>pastarestaurant@totallynotfake.com</p>
+	`;
+	contact.appendChild(contactCard);
+
+	return contact;
+}
+
+function loadContact() {
+	const content = document.querySelector('.content');
+	content.textContent = '';
+	content.appendChild(generateContact());
+}
+
+export default loadContact;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import homeContent from './home.js';
 import loadMenu from './menu.js';
+import loadContact from './contact.js';
 
 const content = document.querySelector('.content')
 const homeButton = document.querySelector('.home-button');
 const menuButton = document.querySelector('.menu-button');
-const aboutButton = document.querySelector('.about-button');
+const contactButton = document.querySelector('.contact-button');
 
 homeButton.addEventListener('click', () => {
   console.log('Hey');
@@ -14,6 +15,6 @@ menuButton.addEventListener('click', () => {
 	loadMenu();
 });
 
-aboutButton.addEventListener('click', () => {
-  console.log('Hey');
+contactButton.addEventListener('click', () => {
+  loadContact();
 });
